6afcd1e12d38b9c1dcf97e35dc34cfa327691f24
Tabea123 <50368069+Tabea123@users.noreply.github.com>
2021-02-03 19:15:08 +0100

    use most recent function for Appell's F1 series

--- PDFDiffBetaFunction.R
+++ PDFDiffBetaFunction.R
@@ -11,6 +11,8 @@
 
 # p = p1 - p2
 
+library(tolerance)
+
 pdf.diff <- function(a1, b1, a2, b2){
   
   logA <- lbeta(a1, b1) + lbeta(a2, b2)
@@ -18,7 +20,7 @@ pdf.diff <- function(a1, b1, a2, b2){
   # for 0 < p =< 1
   smaller1 <- function(p){
     exp(lbeta(a2, b1) + (b1+b2-1)*log(p) + (a2+b1-1)*log(1-p) +
-          log(appell.F1(b1, a1+b1+a2+b2-2, 1-a1, b1+a2, (1-p), (1-p^2))) - logA)
+          log(F1(b1, a1+b1+a2+b2-2, 1-a1, b1+a2, (1-p), (1-p^2))) - logA)
     
   }
   
@@ -30,7 +32,7 @@ pdf.diff <- function(a1, b1, a2, b2){
   plotting <- function(ps){
     
     plot(x = ps, y = mapply(smaller1, ps), xlim = c(-1, 1), ylim = c(0, 6),
-         type = "l", bty = "n", lwd = 2, xlab = "", ylab = "", axes = F,
+         type = "l", bty = "n", lwd = 4, xlab = "", ylab = "", axes = F,
          cex.lab = 1.5, cex.axis = 1.8, cex.main = 1.5, las = 1, bg = "grey")
     
     axis(1, at = seq(-1, 1, 0.25), 
@@ -50,13 +52,13 @@ pdf.diff <- function(a1, b1, a2, b2){
   # for -1 <= p < 0
   smaller0 <- function(p){
     exp(lbeta(a1, b2) + (b1+b2-1)*log(-p) + (a1+b2-1)*log(1+p) +
-          log(appell.F1(b2, 1-a2, a1+a2+b1+b2-2, a1+b2, (1-p^2), (1+p))) - logA)
+          log(F1(b2, 1-a2, a1+a2+b1+b2-2, a1+b2, (1-p^2), (1+p))) - logA)
   }
   
   ps <- seq(-1, -0.005, 0.001)
   
   plot.smaller0 <- lines(x = ps, y = mapply(smaller0, ps), 
-                         xlim = c(-1, 0), type = "l", lwd = 2)
+                         xlim = c(-1, 0), type = "l", lwd = 4)
   
   # p = 0
   point.zero <- points(x = 0,
